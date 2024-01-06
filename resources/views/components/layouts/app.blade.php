a5322831d4be224e82ac8342ff2bdd12f8ff923c
dzackiero <dzakynashshar@gmail.com>
2024-01-07 02:39:28 +0700

    production vite

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,11 +4,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? "Instumen Penilaian Scrum Tim
-        Development" }}</title>
+    <title>{{ $title ??
+        "Instumen Penilaian Scrum Tim
+                        Development" }}</title>
     @wireUiStyles
     @wireUiScripts
     @vite('resources/css/app.css')
+
+    @env('production')
+    <link rel="preload" as="style" href="public/build/assets/app-cV5UvxHO.css">
+    <link rel="stylesheet" href="public/build/assets/app-cV5UvxHO.css">
+    @endenv
+
+    @endif
 </head>
 
 <body>
